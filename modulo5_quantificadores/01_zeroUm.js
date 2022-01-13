ddc89ce96bb67184e515d02a0adfc0d922d819ed
yasminstudent <yasminprog02@gmail.com>
2022-01-13 10:57:20 -0300

    quantificadores

---
+++ modulo5_quantificadores/01_zeroUm.js
@@ -0,0 +1,7 @@
+const texto1 = "De longe eu avistei o fogo e uma pessoa gritando: FOGOOOOOOOO!"
+const texto2 = "There is a big fog in NYC"
+
+// ? -> zero ou um (opcional) ocorrências
+const regex = /fogo?/gi //procure por fog e a letra o é opcional (pois ela esta do lado do interrogação)
+console.log(texto1.match(regex)) // [ 'fogo', 'FOGO' ]
+console.log(texto2.match(regex)) //[ 'fog' ]
