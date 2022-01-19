de3c4c78a4e2ecb87d487fa49ac07adb0337ad2a
Yasmin Pereira da Silva <ysilva@NBUPLEX-4060.uplexis.local>
2022-01-19 09:25:08 -0300

    terminando modulo 8

--- modulo8_receitas/Email.js
+++ modulo8_receitas/Email.js
@@ -2,10 +2,11 @@ const texto = `
 Os e-mails dos convidados são:
     - fulano@cod3r.com.br
     - xico@gmail.com
-    - joao@empresa.info.br
+    - joão@empresa.info.br
     - maria_silva@registro.br
     - rafa.sampaio@yahoo.com
     - fulano+de+tal@escola.ninja.br
 `
 
 console.log(texto.match(/\S+@\w+\.\w{2,6}(\.\w{2})?/g))
+//obs: \S inclui acentos, ponto, _ e etc
--- modulo8_receitas/ipv4.js
+++ modulo8_receitas/ipv4.js
@@ -5,6 +5,7 @@ Inválidos:
 1.333.1.1
 192.168.0.256
 256.256.256.256
+
 Válidos:
 192.168.0.1
 127.0.0.1
@@ -13,7 +14,12 @@ Válidos:
 255.255.255.255
 0.0.0.0
 `
-
-const n = '(\\d{1,2}|1\\d{2}|2[0-4]\\d|25[0-5])'
-const ipv4 = RegExp(`\\b${n}\\.${n}\\.${n}\\.${n}\\b`, 'g')
+/*
+    \\d{1,2} -> captura números de 0 a 99
+    1\\d{2} -> captura números de 100 a 99
+    2[0-4]\\d -> captura números de 200 a 249
+    25[0-5] -> captura números de 250 a 255
+*/
+const n = '(\\d{1,2}|1\\d{2}|2[0-4]\\d|25[0-5])' //como isso é uma string, usamos duas barras
+const ipv4 = RegExp(`\\b${n}\\.${n}\\.${n}\\.${n}\\b`, 'g') //${n}\\. -> número entre 0 a 255 e o ponto literal
 console.log(texto.match(ipv4))
